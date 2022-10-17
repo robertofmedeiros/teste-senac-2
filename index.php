bf92f856661573c5bca81bbafb74a760223bc9f3
Sabrina Damiani <sabrinadamiani88@gmail.com>
2022-10-17 19:51:17 -0300

    commit atualizado

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
     <h1>SENAC</h1>
     <h1>pedrada_aqui</h1>
     <h1>Pico do himalia ketchup maionese gabriel here</h1>
-    <h1>Sabrina Damiani :)</h1>
+    <h1>Sabrina Damiani :) atuakuzado</h1>
 </body>
 </html>
